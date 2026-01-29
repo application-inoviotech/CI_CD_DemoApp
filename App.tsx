fed6d8f324e34500866e8f1d0d5582e21d75d29f
MuhammadSaadSeikh <msaad9124@gmail.com>
2026-01-29 20:09:54 +0500

    fix app file

--- App.tsx
+++ App.tsx
@@ -6,13 +6,17 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
-import { Text } from 'react-native/types_generated/index';
-
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
